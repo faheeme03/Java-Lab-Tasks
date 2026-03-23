5131709eb78996ff6a9693d9d390080f39f55e4a
Mohamed Faheem <157607675+faheeme03@users.noreply.github.com>
2026-03-23 21:24:30 +0530

    Delete week 2 directory

--- week 2/Task6_AllDigitsCount.java
+++
@@ -1,9 +0,0 @@
-public class Task6_AllDigitsCount {
-    public static void main(String[] args) {
-
-        int num = 12345;
-        int count = String.valueOf(num).length();
-
-        System.out.println("Digit Count: " + count);
-    }
-}
